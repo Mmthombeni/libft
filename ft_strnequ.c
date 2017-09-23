0cb1c076c921096f74461dff779dbc1b03402d8d
Malebo MTHOMBENI <mmthombe@e4r6p1.wethinkcode.co.za>
2017-09-23 03:28:20 +0200

    strneq

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: mmthombe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/22 21:55:14 by mmthombe          #+#    #+#             */
-/*   Updated: 2017/09/22 22:07:38 by mmthombe         ###   ########.fr       */
+/*   Updated: 2017/09/23 03:14:07 by mmthombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
+	size_t	i;
 
+	i = 0;
+	if ((s1 == NULL) || (s2 == NULL))
+		return (0);
+	while (s1[i] || s2[i])
+	{
+		if ((s1[i] != s2[i]) && n > i)
+			return (0);
+		i++;
+	}
+	return (1);
 }
